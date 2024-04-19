b32eb5f90b09efa1c954c7e4b3b22a5099005452
Jonas Pereira <66091286+jonasdspereira@users.noreply.github.com>
2024-04-19 00:02:51 +0000

    Animação sombra do card

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      Hello, world!
+      <div className="card">
+        <h2>
+          Rick Sanchez
+        </h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+      </div>
     </>
   )
 }
